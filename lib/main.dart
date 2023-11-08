deeb39fc4bfcc1e3f72fbbe2f27ea082b53798b9
Nicholas Emmanuel <nicholasemmanuel321@gmail.com>
2023-11-08 13:12:06 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('LazyCoders Developers'),
+          centerTitle: true,
+        ),
+        body: const Center(
+          child: Text('Hello Lazy Flutter Developers!'),
+        ),
+      ),
+    ));
