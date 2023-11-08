bd89feda4d5638350436cf4b8470e2bb4d563587
Nicholas Emmanuel <nicholasemmanuel321@gmail.com>
2023-11-08 15:25:55 +0100

    Explored flutter concepts

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,31 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-      home: Scaffold(
+void main() => runApp(const MaterialApp(
+      home: Home(),
+    ));
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
         appBar: AppBar(
           title: const Text('LazyCoders Developers'),
           centerTitle: true,
+          backgroundColor: Colors.red[600],
         ),
         body: const Center(
-          child: Text('Hello Lazy Flutter Developers!'),
+          child: Image(
+            image: AssetImage('assets/nature.jpeg'),
+          ),
         ),
-      ),
-    ));
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            print('You clicked me');
+          },
+          backgroundColor: Colors.red[600],
+          child: const Text('Click'),
+        ));
+  }
+}
